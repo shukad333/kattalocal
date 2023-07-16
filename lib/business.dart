69baf66b8dac7eb06838d97d980deff28d28eb92
Shuhail Kadavath <shukad333@gmail.com>
2023-07-16 15:54:49 +0530

    Add mew Business

--- lib/business.dart
+++ lib/business.dart
@@ -4,34 +4,281 @@ class BusinessPage extends StatefulWidget {
   const BusinessPage({Key? key}) : super(key: key);
 
   @override
-  State<BusinessPage> createState() => _BusinessPageState();
+  State<BusinessPage> createState() => _MyHomePageState();
 }
 
-class _BusinessPageState extends State<BusinessPage> {
+class _MyHomePageState extends State<BusinessPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
+  String firstName = "";
+  String lastName = "";
+  String bodyTemp = "";
+  var measure;
+
+  void _submit() {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: true, // user can tap anywhere to close the pop up
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Your information has been submitted'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                const Align(
+                    alignment: Alignment.topLeft,
+                    child: Text("Full name:",
+                        style: TextStyle(fontWeight: FontWeight.w700))),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(firstName + " " + lastName),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Align(
+                    alignment: Alignment.topLeft,
+                    child: Text("Body Temperature:",
+                        style: TextStyle(fontWeight: FontWeight.w700))),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text("$bodyTemp ${measure == 1 ? "ºC" : "ºF"}"),
+                )
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: Colors.grey,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                  child: const Text('Go to profile'),
+                  onPressed: () {},
+                  // onPressed: () async {
+                  //   FocusScope.of(context)
+                  //       .unfocus(); // unfocus last selected input field
+                  //   Navigator.pop(context);
+                  //   await Navigator.push(
+                  //       context,
+                  //       MaterialPageRoute(
+                  //           builder: (context) =>
+                  //               MyProfilePage())) // Open my profile
+                  //       .then((_) => _formKey.currentState
+                  //       ?.reset()); // Empty the form fields
+                  //   setState(() {});
+                  // }, // so the alert dialog is closed when navigating back to main page
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: Colors.blue,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                  child: const Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Close the dialog
+                    FocusScope.of(context)
+                        .unfocus(); // Unfocus the last selected input field
+                    _formKey.currentState?.reset(); // Empty the form fields
+                  },
+                )
+              ],
+            )
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('New Business'),
+        centerTitle: false,
+        title: const Text("Human IT care"),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.account_circle, size: 32.0),
+            tooltip: 'Profile',
+            onPressed: () {
+              // Navigator.push(
+              //     context,
+              //     MaterialPageRoute(
+              //       builder: (context) => MyProfilePage(),
+              //     ));
+            },
+          ),
+        ],
       ),
       body: Padding(
-        padding: EdgeInsets.all(25),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  IconButton(onPressed: () {}, icon: Icon(Icons.person))
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Text("Enter your Business Details",
+                  style: TextStyle(
+                    fontSize: 24,
+                  )),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        labelText: 'Business Name',
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          borderSide:
+                              BorderSide(color: Colors.grey, width: 0.0),
+                        ),
+                        border: OutlineInputBorder()),
+                    onFieldSubmitted: (value) {
+                      setState(() {
+                        firstName = value.toUpperCase();
+                        // firstNameList.add(firstName);
+                      });
+                    },
+                    onChanged: (value) {
+                      setState(() {
+                        firstName = value.toUpperCase();
+                      });
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty || value.length < 3) {
+                        return 'Business Name must contain at least 3 characters';
+                      } else if (value.contains(RegExp(r'^[0-9_\-=@,\.;]+$'))) {
+                        return 'Business Name cannot contain special characters';
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        labelText: 'Primary Owner Name',
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          borderSide:
+                              BorderSide(color: Colors.grey, width: 0.0),
+                        ),
+                        border: OutlineInputBorder()),
+                    validator: (value) {
+                      if (value == null || value.isEmpty || value.length < 3) {
+                        return 'Primary Name must contain at least 3 characters';
+                      } else if (value.contains(RegExp(r'^[0-9_\-=@,\.;]+$'))) {
+                        return 'Primary Name cannot contain special characters';
+                      }
+                    },
+                    onFieldSubmitted: (value) {
+                      setState(() {
+                        lastName = value.toUpperCase();
+                        // lastNameList.add(lastName);
+                      });
+                    },
+                    onChanged: (value) {
+                      setState(() {
+                        lastName = value.toUpperCase();
+                      });
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        labelText: 'Business Type',
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          borderSide:
+                              BorderSide(color: Colors.grey, width: 0.0),
+                        ),
+                        border: OutlineInputBorder()),
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value == null ||
+                          value.isEmpty ||
+                          value.contains(RegExp(r'^[a-zA-Z\-]'))) {
+                        return 'Use only numbers!';
+                      }
+                    },
+                    onFieldSubmitted: (value) {
+                      setState(() {
+                        bodyTemp = value;
+                        // bodyTempList.add(bodyTemp);
+                      });
+                    },
+                    onChanged: (value) {
+                      setState(() {
+                        bodyTemp = value;
+                      });
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  // DropdownButtonFormField(
+                  //     decoration: const InputDecoration(
+                  //         enabledBorder: OutlineInputBorder(
+                  //           borderRadius:
+                  //           BorderRadius.all(Radius.circular(20.0)),
+                  //           borderSide:
+                  //           BorderSide(color: Colors.grey, width: 0.0),
+                  //         ),
+                  //         border: OutlineInputBorder()),
+                  //     items: [
+                  //       const DropdownMenuItem(
+                  //         child: Text("ºC"),
+                  //         value: 1,
+                  //       ),
+                  //       const DropdownMenuItem(
+                  //         child: Text("ºF"),
+                  //         value: 2,
+                  //       )
+                  //     ],
+                  //     hint: const Text("Select item"),
+                  //     onChanged: (value) {
+                  //       setState(() {
+                  //         measure = value;
+                  //         // measureList.add(measure);
+                  //       });
+                  //     },
+                  //     onSaved: (value) {
+                  //       setState(() {
+                  //         measure = value;
+                  //       });
+                  //     }),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: const Size.fromHeight(60)),
+                    onPressed: () {
+                      // Validate returns true if the form is valid, or false otherwise.
+                      if (_formKey.currentState!.validate()) {
+                        _submit();
+                      }
+                    },
+                    child: const Text("Submit"),
+                  ),
                 ],
               ),
-              const SizedBox(
-                height: 40,
-              ),Container(padding: EdgeInsets.symmetric(horizontal: 10,vertical: 5),child: TextFormField(
-                style: TextStyle(color: Theme.of(context).primaryColor),
-              ),)
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
