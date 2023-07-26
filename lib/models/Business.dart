b124049a841b563db8fd3c4e3a7ef676d6067e4d
Shuhail Kadavath <shukad333@gmail.com>
2023-07-26 18:46:46 +0530

    business model

---
+++ lib/models/Business.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Business {
+  final String id;
+  final String name;
+  final String type;
+
+  Business({required this.id,required this.name,required this.type});
+
+
+}
