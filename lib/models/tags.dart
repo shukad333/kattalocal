2c61107f66bea98ed95b38bb626e383116c19e97
Shuhail Kadavath <shukad333@gmail.com>
2023-09-12 21:49:35 +0530

    tags model , serialize and deserialize

---
+++ lib/models/tags.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Tag {
+  final int id;
+  final String name;
+
+  Tag({required this.id, required this.name});
+
+  factory Tag.fromJson(Map<String, dynamic> json) {
+    return Tag(id: json['id'], name: json['name']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'name': name};
+  }
+}
